18215c249cd2c4542bfefd761c5c44a72a5ff953
Weltbild <111874406+nts-bot@users.noreply.github.com>
2022-09-15 18:04:24 +0200

    .

--- py/script.py
+++ py/script.py
@@ -497,6 +497,7 @@ class nts:
             if kind == 'search': # WIP
                 # pass
                 req = self.mt_spotifysearch(eval(jsonlist[0]),multiple)
+<<<<<<< HEAD
             return(req)
             # elif kind == 'bandcamp':
             #     req = self.mt_bandcamp(eval(jsonlist[0]),eval(jsonlist[2]),multiple)
@@ -504,6 +505,14 @@ class nts:
             #     for td in multiple[episode]:
             #         eval(jsonlist[1])[episode][td] = req[episode][td]
             # self._d2j(f'./{jsonlist[1]}/{show}',eval(jsonlist[1]))
+=======
+            elif kind == 'bandcamp':
+                req = self.mt_bandcamp(eval(jsonlist[0]),eval(jsonlist[2]),multiple)
+            for episode in multiple:
+                for td in multiple[episode]:
+                    eval(jsonlist[1])[episode][td] = req[episode][td]
+            self._d2j(f'./{jsonlist[1]}/{show}',eval(jsonlist[1]))
+>>>>>>> parent of d3f6245 (.)
 
     def spotifysearch(self,showson,episode,trdx):
         q0= f'artist:{showson[episode][trdx]["artist"]} track:{showson[episode][trdx]["title"]}'
@@ -901,7 +910,14 @@ class nts:
                     raise RuntimeError('DICTIONARY UPDATE SCRIPT FAILED')
         return(old)
         
+<<<<<<< HEAD
     # MULTITHREADING FUNCTIONS
+=======
+    # MULTITHREADING
+
+    def mt_spotipy(self,query):
+        return(self._run(query))
+>>>>>>> parent of d3f6245 (.)
 
     def mt_request(self,content):
         c = 0
@@ -916,6 +932,57 @@ class nts:
                 print(f'. . . . .RE:{c}.',end='\r')
                 time.sleep(1.0)
 
+<<<<<<< HEAD
+=======
+    def multithreading(self,tasklist, no_workers,task):
+        class __worker__(Thread):
+            def __init__(self, request_queue):
+                Thread.__init__(self)
+                self.queue = request_queue
+                self.results = []
+
+            def run(self):
+                while True:
+                    content = self.queue.get()
+                    if content == "":
+                        break
+                    if not isinstance(content,list):
+                        content = [content]
+                    start = time.time()
+                    # TASK START
+                    response = eval(f'{task}(*{content})')
+                    # TASK END
+                    end = time.time()
+                    print(round(end - start,4),end='\r')
+                    self.results.append(response)
+                    self.queue.task_done()
+
+        # Create queue and add tasklist
+        q = queue.Queue()
+        for url in tasklist:
+            q.put(url)
+
+        # Workers keep working till they receive an empty string
+        for _ in range(no_workers):
+            q.put("")
+
+        # Create workers and add tot the queue
+        workers = []
+        for _ in range(no_workers):
+            worker = __worker__(q)
+            worker.start()
+            workers.append(worker)
+        # Join workers to wait till they finished
+        for worker in workers:
+            worker.join()
+
+        # Combine results from all workers
+        r = []
+        for worker in workers:
+            r.extend(worker.results)
+        return r
+
+>>>>>>> parent of d3f6245 (.)
     def mt_spotifysearch(self,showson,multiple):
 
         q1 = dict()
@@ -937,7 +1004,11 @@ class nts:
 
         # run syncronious mass request
         time.sleep(1.0)
+<<<<<<< HEAD
         response = multithreading(Q1+Q2, 16, 'spotify') #_run
+=======
+        response = self.multithreading(Q1+Q2, 16, 'self.mt_spotipy')
+>>>>>>> parent of d3f6245 (.)
         if not isinstance(response,list):
             response = [response]
 
@@ -961,34 +1032,35 @@ class nts:
 
         qk = list(q1.keys())
         print(f'.{len(r1)}/{len(r2)}->{len(p1)}/{len(p2)}={len(qk)}.')
+        for episode in range(len(qk)):
 
-        return(p1,p2,qk)
+            qt = list(q1[qk[episode]].keys())
+            print(f'.{len(p1[episode])}/{len(p2[episode])}={len(qt)}.',end='\r')
 
+<<<<<<< HEAD
         # for episode in range(len(qk)): # WIP
+=======
+            for td in range(len(qt)):
+>>>>>>> parent of d3f6245 (.)
 
-        #     qt = list(q1[qk[episode]].keys())
-        #     print(f'.{len(p1[episode])}/{len(p2[episode])}={len(qt)}.',end='\r')
+                if p1[qk[episode]][qt[td]]['tracks']['items']:
+                    S0 = [{'artist':j['artists'][0]['name'],
+                        'title':j['name'],
+                        'uri':j['uri'].split(':')[-1]} 
+                        for j in p1[qk[episode]][qt[td]]['tracks']['items'][:3]]
+                else:
+                    S0 = ''
+                if p2[qk[episode]][qt[td]]['tracks']['items']:
+                    S1 = [{'artist':j['artists'][0]['name'],
+                        'title':j['name'],
+                        'uri':j['uri'].split(':')[-1]} 
+                        for j in p2[qk[episode]][qt[td]]['tracks']['items'][:3]]
+                else:
+                    S1 = ''
 
-        #     for td in range(len(qt)):
+                q1[qk[episode]][qt[td]] = {'s0':S0,'s1':S1}
 
-        #         if p1[td][qk[episode]][qt[td]]['tracks']['items']:
-        #             S0 = [{'artist':j['artists'][0]['name'],
-        #                 'title':j['name'],
-        #                 'uri':j['uri'].split(':')[-1]} 
-        #                 for j in p1[qk[episode]][qt[td]]['tracks']['items'][:3]]
-        #         else:
-        #             S0 = ''
-        #         if p2[td][qk[episode]][qt[td]]['tracks']['items']:
-        #             S1 = [{'artist':j['artists'][0]['name'],
-        #                 'title':j['name'],
-        #                 'uri':j['uri'].split(':')[-1]} 
-        #                 for j in p2[qk[episode]][qt[td]]['tracks']['items'][:3]]
-        #         else:
-        #             S1 = ''
-
-        #         q1[qk[episode]][qt[td]] = {'s0':S0,'s1':S1}
-
-        # return(q1)
+        return(q1)
 
     def mt_bandcamp(self,showson,rateson,multiple):
         
@@ -1029,7 +1101,11 @@ class nts:
 
         # run syncronious mass request
         time.sleep(1.0)
+<<<<<<< HEAD
         response = multithreading(querylist, 16, 'bandcamp')
+=======
+        response = self.multithreading(querylist, 16, 'self.mt_request')
+>>>>>>> parent of d3f6245 (.)
 
         # make nested list
         if not isinstance(response,list):
@@ -1208,6 +1284,7 @@ class nts:
         with open(f"./html/{show}.html", 'w', encoding='utf8') as f:
             f.write(pretty)
 
+<<<<<<< HEAD
 # END
 
 # MULTITHREADING WORKER
@@ -1268,3 +1345,58 @@ def multithreading(tasklist, no_workers,kind):
     for worker in workers:
         r.extend(worker.results)
     return r
+=======
+# OLD
+
+# class working:
+
+#     def __init__(self,task,extraArgs):
+#         # The queue for tasks
+#         self.q = queue.Queue()
+#         self.task = task
+#         self.extraArgs = extraArgs
+#         self.results = []
+#     # Worker, handles each task
+#     def worker(self):
+#         while True:
+#             item = self.q.get()
+#             if item is None:
+#                 break
+#             print("Working on", item, end='\r')
+#             # START TASK FUNCTION
+#             self.results += [eval(f'{self.task}(*{self.extraArgs})')]
+#             # END TASK FUNCTION
+#             time.sleep(1)
+#             self.q.task_done()
+#     def start_workers(self,worker_pool=1000):
+#         threads = []
+#         for i in range(worker_pool):
+#             t = Thread(target=i)
+#             t.start()
+#             threads.append(t)
+#         return threads
+#     def stop_workers(self,threads):
+#         # stop workers
+#         for i in threads:
+#             self.q.put(None)
+#         for t in threads:
+#             t.join()
+#     def create_queue(self,task_items):
+#         for item in task_items:
+#             self.q.put(item)
+
+
+#     def run(self):
+#         # Dummy tasks
+#         tasks = [item for item in range(1000)]
+
+#         # Start up your workers
+#         workers = self.start_workers(worker_pool=10)
+#         self.create_queue(tasks)
+
+#         # Blocks until all tasks are complete
+#         self.q.join()
+#         self.stop_workers(workers)
+
+#
+>>>>>>> parent of d3f6245 (.)
