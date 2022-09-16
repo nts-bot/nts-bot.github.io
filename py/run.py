96ec7567b21fb5b6d19f40edfdda8579e24c17bb
Weltbild <111874406+nts-bot@users.noreply.github.com>
2022-09-16 18:32:45 +0200

    .

--- py/run.py
+++ py/run.py
@@ -8,7 +8,7 @@ os.chdir(f"{dr}/py")
 import script
 nts = script.nts()
 
-am = 50 #10 # 50 ~ a week of shows
+am = 10 #10 # 50 ~ a week of shows
 
 soup = nts.browse("https://www.nts.live/latest",amount=am)
 episodes = soup.select('a.nts-grid-v2-item__header')
